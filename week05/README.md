3f54c4ea7f38173f41ef186b64a49255452311a9
CyberTDan <trihlebdv@gmail.com>
2023-10-11 19:46:20 +0300

    Add slution to 5th lab

---
+++ week05/README.md
@@ -0,0 +1,62 @@
+# ERC20 Token Exercise
+
+## Contract code 
+
+can be found in 'token.sol' file
+
+<br>
+
+## Application 
+
+Application code placed in 'app' folder. This application implement simple html page for transfering and checking balance
+
+<br>
+
+## Execution
+
+Application was launched using 'Ganache' and 'Remix'. One can open it on http://127.0.0.1:500
+
+<br>
+
+Enterance page:
+
+![start page](img/1.jpg)
+
+<br>
+
+Ganache created users:
+
+![start page](img/2.jpg)
+
+First user is contract owner
+
+
+<br>
+
+Initial coin balance:
+
+![start page](img/3.jpg)
+
+<br>
+
+Transfer from first to second user 100 coins:
+
+![start page](img/4.jpg)
+
+<br>
+
+Contract owner balance reduced on 100+50 coins:
+
+![start page](img/5.jpg)
+
+<br>
+
+Receiver user balance become 100:
+
+![start page](img/6.jpg)
+
+<br>
+
+Professor balance increased on 50 coins:
+
+![start page](img/7.jpg)
---
+++ week05/app/app.py
@@ -0,0 +1,291 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+from web3 import Web3
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+# Connect to a local Ethereum node
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
+
+print(w3.api)
+
+# Ensure connection
+if not w3.is_connected():
+    raise ValueError('Cannot connect to local Ethereum node.')
+
+# SimpleERC20 ABI (You need to extract this from your compiled contract)
+simple_erc20_abi = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": False,
+		"inputs": [
+			{
+				"indexed": True,
+				"internalType": "address",
+				"name": "tokenOwner",
+				"type": "address"
+			},
+			{
+				"indexed": True,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": False,
+				"internalType": "uint256",
+				"name": "tokens",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "delegate",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "numTokens",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "receiver",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "numTokens",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": False,
+		"inputs": [
+			{
+				"indexed": True,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": True,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": False,
+				"internalType": "uint256",
+				"name": "tokens",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "buyer",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "numTokens",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "delegate",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "tokenOwner",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+# Contract address (Replace with your deployed contract address)
+contract_address = '0xb4356E49aD45E7477e53BE330Df7Ce8F67cb4ca2'
+contract = w3.eth.contract(address=contract_address, abi=simple_erc20_abi)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/balance', methods=['POST'])
+def get_balance():
+    address = request.form.get('address')
+    try:
+        balance = contract.functions.balanceOf(address).call()
+        return render_template('balance_result.html', address=address, balance=balance)
+    except Exception as e:
+        flash(f'Error fetching balance: {str(e)}', 'error')
+        return redirect(url_for('index'))
+
+@app.route('/transfer', methods=['POST'])
+def transfer():
+    # Extract data from the form
+    to_address = request.form.get('to_address')
+    amount = int(request.form.get('amount'))
+    from_address = request.form.get('from_address')
+    private_key = request.form.get('private_key')
+
+    # Create a transaction
+    nonce = w3.eth.get_transaction_count(from_address)
+    tx = {
+        'nonce': nonce,
+        'to': contract_address,
+        'gas': 2000000,
+        'gasPrice': w3.to_wei('50', 'gwei'),
+        'data': contract.functions.transfer(to_address, amount).build_transaction({'chainId': 1, 'gas': 2000000, 'gasPrice': w3.to_wei('50', 'gwei'), 'nonce': nonce})['data']
+    }
+
+    # Sign and send the transaction
+    signed_tx = w3.eth.account.sign_transaction(tx, private_key)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    flash('Transfer initiated!', 'success')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
